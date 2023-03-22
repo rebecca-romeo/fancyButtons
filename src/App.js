50068e6762ebebcd4c268cd264bad8143ad14b44
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-03-22 16:53:03 -0400

    button project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import LightSwitchButton from './components/ LightSwitchButton';
+import AngryButton from './components/AngryButton';
+import CounterButton from './components/CounterButton';
+import TextRepeaterButton from './components/TextRepeaterButton';
 
 function App() {
+  const [light, setLight] = useState('off');
+  const switchLight = () => setLight(light === "on" ? "off" : "on");
+  const dark = (light === 'off') ? 'dark' : '';
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${dark}`}>
+      <h1>Fancy Buttons!</h1>
+      <section>
+        <AngryButton />
+        <CounterButton />
+        <LightSwitchButton
+          light={light}
+          switchLight={switchLight}/>
+        <TextRepeaterButton />
+      </section>
     </div>
   );
 }
